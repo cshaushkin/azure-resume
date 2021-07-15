ace578710a85f0eeba3a626e7c56c845fd7bd53e
Hammer & Code <chris@hammerandcode.com>
2021-07-14 18:31:56 -0700

    updates

---
+++ frontend/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMCOntentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 42;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called funtion API");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
